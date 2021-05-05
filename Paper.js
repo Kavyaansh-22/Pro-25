5ebbfec3e58a9360e7b0ecd26da6a40473f63e12
Kavyaansh-22 <76993698+Kavyaansh-22@users.noreply.github.com>
2021-05-05 11:11:26 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,34 @@
+class Paper{
+  constructor(x,y,r){
+
+    var options = {
+        
+        'isStatic' : false,
+        'restitution' : 0.3,
+        'friction' :0,
+        'density' :1.2
+
+    }
+    this.image = loadImage("paper.png");
+    this.body= Bodies.circle(x,y,r,options);
+    this.x = x;
+    this.y = y;
+    this.r = r; 
+
+    World.add(world, this.body);
+
+  }
+  display()
+	{
+			var paperpos=this.body.position;		
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)			
+			fill("white");		
+            ellipse(0,0,this.r,this.r);
+
+
+			pop()
+	}
+
+}
