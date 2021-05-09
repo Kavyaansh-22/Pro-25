a7ff704e35f13ce4bd928da8cd68cf114f4324cb
Kavyaansh-22 <76993698+Kavyaansh-22@users.noreply.github.com>
2021-05-09 17:11:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,9 +4,8 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-var dustbinObj,groundObject	
+var dustbinObj, paperObject,groundObject	
 var world;
-var paper,paperObject;	
 
 
 function setup() {
@@ -17,11 +16,24 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 	
+	paperObject=new paper(200,450,70);
 	groundObject=new ground(width/2,670,width,20);
 	dustbinObj=new dustbin(1200,650);
-	paperObject = new Paper(200,200,50);
+	//Create a Ground
+	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1600,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
 
 	Engine.run(engine);
+	Render.run(render);
   
 }
 
@@ -29,21 +41,25 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(230);
-
+ 
   
-
-  keyPressed();
+  paperObject.display();
   groundObject.display();
   dustbinObj.display();
-  paperObject.display();
+
   
-
+  
+ 
+  
+  
+ 
 }
 
-function keyPressed(){
-	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(paperObject.body,paperObject.body.position, {x:130,y:-145});
-		
-	}
-}
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
 
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+
+    
+  	}
+}
