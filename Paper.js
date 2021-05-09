cd5bacd01bec827ad0e69367e745be06ed1fd2bc
Kavyaansh-22 <76993698+Kavyaansh-22@users.noreply.github.com>
2021-05-09 17:10:47 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,35 +1,38 @@
-class Paper{
-  constructor(x,y,r){
-
-    var options = {
-        
-        'isStatic' : false,
-        'restitution' : 0.1,
-        'friction' :0,
-        'density' :7.5
-
-    }
-    this.image = loadImage("paper.png");
-    this.body= Bodies.circle(x,y,r,options);
-    this.x = x;
-    this.y = y;
-    this.r = r; 
-
-    World.add(world, this.body);
-
-  }
-  display()
+class paper
+{
+	constructor(x,y,r)
 	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("paper.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
 			var paperpos=this.body.position;		
+
 			push()
-			//translate(paperpos.x, paperpos.y);
-			rectMode(CENTER)			
-      image(this.image,paperpos.x,paperpos.y,this.r*1,this.r*1);
-			//fill("white");		
-        //    ellipse(0,0,this.r,this.r); 
-
-
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+			//ellipse(0,0,this.r, this.r);
 			pop()
+			
 	}
 
 }
